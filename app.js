267be36658d5148d65751e4d9963f81f426ce471
SebasIBR <94653681+SebasIBR@users.noreply.github.com>
2022-12-29 10:50:43 -0500

    Montaje del servidor local

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname,"./views/home.html"))
+})
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(3000,()=>{
+    console.log("servidor corriendo en el puerto 300");
+})
