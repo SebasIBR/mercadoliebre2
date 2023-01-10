57056b79095255509834928c0b64fea9c5ea62ef
SebasIBR <94653681+SebasIBR@users.noreply.github.com>
2023-01-10 15:48:53 -0500

    Posicionamiento avanzado parte 1

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ const app = express();
 const path = require('path');
 
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname,"./views/home.html"))
-})
-
-
+const actualizacion = false
+if (actualizacion == false){
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname,"./views/home.html"))
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send("Sitio en reparacion")
+    })
+}
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.listen(3000,()=>{
-    console.log("servidor corriendo en el puerto 3000");
+const server = true
+if (server == true) {
+app.listen(80,()=>{
+    console.log("servidor corriendo en el puerto 80");
 })
+} else {
+    app.listen(3000,()=>{
+        console.log("servidor corriendo en el puerto 3000");
+    })
+}
